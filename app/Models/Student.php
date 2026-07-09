919c69f3b92410f23ba12c9afc4b37128a378166
vakhimkrean <vakhim.krean@student.passerellesnumeriques.org>
2026-07-09 09:41:55 +0700

    add-student-model

---
+++ app/Models/Student.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Student extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'student_code',
+        'batch_id',
+        'tutor_id',
+        'name',
+        'gender',
+        'phone',
+        'email',
+        'photo',
+        'status',
+    ];
+
+    public function batch(): BelongsTo
+    {
+        return $this->belongsTo('App\Models\Batch');
+    }
+
+    public function tutor(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'tutor_id');
+    }
+}
