455b66ba29608ec015d7d49c9f63d87c94a867d5
vakhimkrean <vakhim.krean@student.passerellesnumeriques.org>
2026-07-09 10:57:06 +0700

    feat: #30 Implement StudentController@destroy

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -69,6 +69,9 @@ class StudentController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $student = Student::findOrFail($id);
+        $student->delete();
+
+        return response()->noContent();
     }
 }
