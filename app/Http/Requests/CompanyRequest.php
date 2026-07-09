67630743e9f736a874eadcd987e972d6cbe2633a
Serey002 <serey.phem.12022006@gmail.com>
2026-07-09 09:25:50 +0700

    feat: #6 create CompanyRequest for input validation

---
+++ app/Http/Requests/CompanyRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CompanyRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'company_name' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('companies', 'company_name')->ignore($this->route('company')),
+            ],
+            'address' => ['nullable', 'string', 'max:255'],
+            'industry' => ['nullable', 'string', 'max:255'],
+            'contact_person' => ['nullable', 'string', 'max:255'],
+            'phone' => ['nullable', 'string', 'max:50'],
+            'email' => ['nullable', 'email', 'max:255'],
+            'website' => ['nullable', 'url', 'max:255'],
+        ];
+    }
+}
