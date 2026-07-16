35f28eac6d1d3043e17dce30a2a7f63426859d46
Danemiok <kenk514912@gmail.com>
2026-07-16 11:05:25 +0700

    style: use casts property instead of method in Student model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Student extends Model
 {
@@ -26,14 +26,11 @@ class Student extends Model
         'status',
     ];
 
-    protected function casts(): array
-    {
-        return [
-            'batch_id' => 'integer',
-            'tutor_id' => 'integer',
-            'user_id' => 'integer',
-        ];
-    }
+    protected $casts = [
+        'batch_id' => 'integer',
+        'tutor_id' => 'integer',
+        'user_id' => 'integer',
+    ];
 
     public function batch(): BelongsTo
     {
