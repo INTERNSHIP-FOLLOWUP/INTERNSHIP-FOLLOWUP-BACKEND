f8c5b0036996cbf870a70796476aef0c27867562
Danemiok <kenk514912@gmail.com>
2026-07-20 14:35:14 +0700

    fix: update tutor seeder to use first_name/last_name

--- database/seeders/TutorSeeder.php
+++ database/seeders/TutorSeeder.php
@@ -18,28 +18,21 @@ class TutorSeeder extends Seeder
         }
 
         $tutors = [
-            [
-                'name'  => 'HEY him',
-                'email' => 'hey.him@tutor.com',
-            ],
-            [
-                'name'  => 'YEN yon',
-                'email' => 'yen.yon@tutor.com',
-            ],
-            [
-                'name'  => 'MENG HEANG',
-                'email' => 'meng.heang@tutor.com',
-            ],
+            ['first_name' => 'Hey',   'last_name' => 'Him',     'email' => 'hey.him@tutor.com'],
+            ['first_name' => 'Yen',   'last_name' => 'Yon',     'email' => 'yen.yon@tutor.com'],
+            ['first_name' => 'Meng',  'last_name' => 'Heang',   'email' => 'meng.heang@tutor.com'],
         ];
 
         foreach ($tutors as $tutorData) {
             User::firstOrCreate(
                 ['email' => $tutorData['email']],
                 [
-                    'name'     => $tutorData['name'],
-                    'email'    => $tutorData['email'],
-                    'password' => '12345678',
-                    'role_id'  => $role->id,
+                    'first_name' => $tutorData['first_name'],
+                    'last_name'  => $tutorData['last_name'],
+                    'email'      => $tutorData['email'],
+                    'password'   => '12345678',
+                    'role_id'    => $role->id,
+                    'theme'      => 'light',
                 ]
             );
         }
