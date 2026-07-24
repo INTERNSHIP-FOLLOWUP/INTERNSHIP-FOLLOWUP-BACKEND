d83c0aa468378df0fef4e6c56482b267c92b94e8
Serey002 <serey.phem.12022006@gmail.com>
2026-07-24 18:50:25 +0700

    fix companyseeder

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -15,38 +15,24 @@ class CompanySeeder extends Seeder
                 'company_name' => 'Tech Solutions Cambodia',
                 'address' => 'Phnom Penh, Cambodia',
                 'industry' => 'Information Technology',
-                'contact_person' => 'Sokha Chea',
-                'phone' => '012 345 678',
                 'email' => 'info@techsolutions-kh.com',
                 'website' => 'https://techsolutions-kh.com',
-                'user_email' => 'company@gmail.com',
+                'company_image_url' => '/images/companies/tech-solutions.png',
             ],
             [
                 'company_name' => 'Digital Innovation Lab',
                 'address' => 'Siem Reap, Cambodia',
                 'industry' => 'Digital Marketing',
-                'contact_person' => 'Rithy Chan',
-                'phone' => '098 765 432',
                 'email' => 'contact@digital-lab-kh.com',
                 'website' => 'https://digital-lab-kh.com',
-                'user_email' => null,
+                'company_image_url' => '/images/companies/digital-lab.png',
             ],
         ];
 
         foreach ($companies as $data) {
-            $companyData = [
-                'company_name' => $data['company_name'],
-                'address' => $data['address'],
-                'industry' => $data['industry'],
-                'contact_person' => $data['contact_person'],
-                'phone' => $data['phone'],
-                'email' => $data['email'],
-                'website' => $data['website'],
-            ];
-
             Company::firstOrCreate(
                 ['company_name' => $data['company_name']],
-                $companyData
+                $data
             );
         }
     }
